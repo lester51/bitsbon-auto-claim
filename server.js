630ea47ed572101ee82489852f63e018bcdc6a87
lester51 <lesternavarra@gmail.com>
2025-02-05 23:31:24 +0800

    Bug Fix

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const colors = require('colors');
-const { displayHeader, accinfo, login } = require('./src/index');
-require('dotenv').config();
-
 class Server {
     async startServer() {
+        const colors = require('colors');
+        const path = require('path'); 
+        const express = require('express');
+        const {
+            displayHeader,
+            accinfo,
+            login,
+            spin
+        } = require('./src/index');
+        require('dotenv').config({
+            path: path.resolve(__dirname, './.env')
+        });
         const username = process.env.USERNAME;
         const password = process.env.PASSWORD;
         const port = process.env.PORT || 3000;
-
         colors.setTheme({
             silly: 'rainbow',
             input: 'grey',
@@ -23,16 +29,44 @@ class Server {
         });
 
         const app = express();
+        
+        function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        async function runTasks() {
+            try {
+                let cookies = await login(username, password);
+                let resp = await spin(cookies);
+                if (typeof resp.message === 'undefined') throw new Error("Please wait until the timer runs out and try again later.");
+                console.log(colors.info.bold("[ SCRIPT ] ")+colors.info(resp.message))
+                await accinfo(cookies, true);
+            }
+            catch(e) {
+                console.log(colors.warn.bold("[ SCRIPT ] ")+colors.error(e.error))
+                console.log(colors.warn.bold("[ SCRIPT ] ")+colors.warn("Reconnecting..."))
+                await delay(60000);
+                await runTasks();
+            }
+        }
+        
+        async function runEveryTenMinutes() {
+            while (true) {
+                await runTasks();
+                await delay(600000);
+            }
+        }
 
         app.get('/', (req, res) => {
-            res.send('SERVER FOR BITBON FAUCET - AUTO FARM SCRIPT\nMADE\nBY\nHackMeSenpai(HMS)');
+            res.send('SERVER FOR BITBON FAUCET - AUTO FARM SCRIPT\nMADE\nBY\nHackMeSenpai(HMS)')
         });
-
-        app.listen(port, async () => {
+        
+        app.listen(port, async() => {
             displayHeader();
-            console.log(colors.verbose.bold("[ SERVER ]") + colors.info(` Server port ${port} exposed!`));
+            console.log(colors.verbose.bold("[ SERVER ]")+colors.info(` Server port ${port} exposed!`));
             let cookies = await login(username, password);
             await accinfo(cookies);
+            runEveryTenMinutes();
         });
     }
 }
